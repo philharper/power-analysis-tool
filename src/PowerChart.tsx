4f1123891cfc44579dd3a81afb9bfc9c86e6b76f
Phil Harper <philiph@motabilityoperations.co.uk>
2020-05-22 17:36:21 +0100

    Add button to generate graph and small refactors

--- src/PowerChart.tsx
+++ src/PowerChart.tsx
@@ -8,32 +8,40 @@ import {
   } from '@devexpress/dx-react-chart-material-ui';
 import {DropzoneArea} from 'material-ui-dropzone';
 import {xml2js} from 'xml-js'
+import { Button } from '@material-ui/core';
 
 function PowerChart() {
 
     const [data, setData] = useState([] as any);
+    const [files, setFiles] = useState([] as File[]);
 
     const uploadFile = async (acceptedFiles: File[]) => {
-        let powerReadings: { time: number; power: number; }[] = [];
+        setFiles(acceptedFiles);
+    }
 
-        for (const acceptedFile of acceptedFiles) {
+    const generateGraph = async () => {
+        let powerReadings: { 
+            time: number; 
+            power: number;
+        }[] = [];
+
+        for (const acceptedFile of files) {
             await acceptedFile.text().then((output) => {
                 const gpxContent = xml2js(output);
                 let i = 0;
                 for (const reading of gpxContent.elements[0].elements[1].elements[2].elements) {
                     const time = reading.elements[1].elements[0].text;
                     const power = reading.elements[2].elements[0].elements[0].text;
-
-                    i++;
-                    const powerReading = {
-                        'time' : i,
-                        'power' : parseInt(power)
-                    }
-
-                    console.log(powerReading);
                     
-                    if (!isNaN(powerReading.power))
+                    if (!isNaN(power)) {
+                        i++;
+                        const powerReading = {
+                            time : i,
+                            power : parseInt(power)
+                        }
+
                         powerReadings.push(powerReading);
+                    }
                 }
             });
         }
@@ -50,6 +58,7 @@ function PowerChart() {
                     dropzoneText={''}
                 />
             </div>
+            <Button onClick={generateGraph}>Compare</Button>
             <Chart
                 data={data}
             >
