6625d94d64a46a35845c6443d19ae7703ae065cd
Phil Harper <philiph@motabilityoperations.co.uk>
2020-05-22 16:46:15 +0100

    First working e2e

--- src/PowerChart.tsx
+++ src/PowerChart.tsx
@@ -4,6 +4,7 @@ import {
     ValueAxis,
     Chart,
     LineSeries,
+    ZoomAndPan
   } from '@devexpress/dx-react-chart-material-ui';
 import {DropzoneArea} from 'material-ui-dropzone';
 import {xml2js} from 'xml-js'
@@ -13,21 +14,26 @@ function PowerChart() {
     const [data, setData] = useState([] as any);
 
     const uploadFile = async (acceptedFiles: File[]) => {
-        let powerReadings: { time: any; power: any; }[] = [];
+        let powerReadings: { time: number; power: number; }[] = [];
 
         for (const acceptedFile of acceptedFiles) {
             await acceptedFile.text().then((output) => {
                 const gpxContent = xml2js(output);
+                let i = 0;
                 for (const reading of gpxContent.elements[0].elements[1].elements[2].elements) {
                     const time = reading.elements[1].elements[0].text;
                     const power = reading.elements[2].elements[0].elements[0].text;
 
+                    i++;
                     const powerReading = {
-                        'time' : time,
-                        'power' : power
+                        'time' : i,
+                        'power' : parseInt(power)
                     }
 
-                    powerReadings.push(powerReading);
+                    console.log(powerReading);
+                    
+                    if (!isNaN(powerReading.power))
+                        powerReadings.push(powerReading);
                 }
             });
         }
@@ -37,16 +43,20 @@ function PowerChart() {
 
     return (
         <>
-            <DropzoneArea
-                onChange={uploadFile}
-                acceptedFiles={['.gpx']}
-            />
+            <div style={{height: 300, width: 300}}>
+                <DropzoneArea
+                    onChange={uploadFile}
+                    acceptedFiles={['.gpx']}
+                    dropzoneText={''}
+                />
+            </div>
             <Chart
                 data={data}
             >
                 <ArgumentAxis />
                 <ValueAxis />
                 <LineSeries valueField="power" argumentField="time" />
+                <ZoomAndPan />
             </Chart>
         </>
     )
