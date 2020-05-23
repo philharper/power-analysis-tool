b546eefd28f3491ba16c68bdd2e49fcf6a5982f8
Harper <harper@harpers-air.home>
2020-05-23 15:53:28 +0100

    Refactors

--- src/PowerChart.tsx
+++ src/PowerChart.tsx
@@ -4,11 +4,17 @@ import {
     ValueAxis,
     Chart,
     LineSeries,
-    ZoomAndPan
+    ZoomAndPan,
+    Legend
   } from '@devexpress/dx-react-chart-material-ui';
 import {DropzoneDialog} from 'material-ui-dropzone';
 import {xml2js} from 'xml-js'
 import { Button } from '@material-ui/core';
+
+class DataEntry {
+    time?: number;
+    power?: number;
+}
 
 function PowerChart() {
 
@@ -16,10 +22,9 @@ function PowerChart() {
     const [files, setFiles] = useState([] as File[]);
     const [uploadOpen, setUploadOpen] = useState(false);
 
-    const uploadFile = async (acceptedFiles: File[]) => {
+    const uploadFile = (acceptedFiles: File[]) => {
         setFiles(acceptedFiles);
         setUploadOpen(false);
-        generateGraph();
     }
 
     const openUploadDialog = () => {
@@ -31,45 +36,32 @@ function PowerChart() {
     }
 
     const generateGraph = async () => {
-        let powerReadings: { 
-            time: number; 
-            power: number;
-        }[] = [];
+        let powerReadings: DataEntry[] = [];
 
         for (const acceptedFile of files) {
             await acceptedFile.text().then((output) => {
                 const gpxContent = xml2js(output);
-                let i = 0;
                 for (const reading of gpxContent.elements[0].elements[1].elements[2].elements) {
-                    const time = reading.elements[1].elements[0].text;
+                    const time = new Date(reading.elements[1].elements[0].text);
                     const power = reading.elements[2].elements[0].elements[0].text;
                     
                     if (!isNaN(power)) {
-                        i++;
-                        const powerReading = {
-                            time : i,
-                            power : parseInt(power)
-                        }
-
+                        const powerReading = new DataEntry() 
+                        powerReading.time = time.getTime();
+                        powerReading.power = parseInt(power);
                         powerReadings.push(powerReading);
                     }
                 }
             });
         }
-
+        
         setData(powerReadings);
     }
 
     return (
         <>
-            <DropzoneDialog
-                open={uploadOpen}
-                onSave={uploadFile}
-                acceptedFiles={['.gpx']}
-                dropzoneText={''}
-                onClose={closeUploadDialog}
-            />
             <Button onClick={openUploadDialog}>Upload File</Button>
+            <Button onClick={generateGraph}>Generate Graph</Button>
             <Chart
                 data={data}
             >
@@ -77,7 +69,15 @@ function PowerChart() {
                 <ValueAxis />
                 <LineSeries valueField="power" argumentField="time" />
                 <ZoomAndPan />
+                <Legend />
             </Chart>
+            <DropzoneDialog
+                open={uploadOpen}
+                onSave={uploadFile}
+                acceptedFiles={['.gpx']}
+                dropzoneText={''}
+                onClose={closeUploadDialog}
+            />
         </>
     )
 }
