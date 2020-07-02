8d7ee5ddad1524f68c07ebde4ccb9b2b35dea802
Phil Harper <philiph@motabilityoperations.co.uk>
2020-07-02 20:19:44 +0100

    Fix tests and remove DataEntry constructor

--- src/DataEntry.ts
+++ src/DataEntry.ts
@@ -2,9 +2,4 @@ export default class DataEntry {
     time?: Date;
     power1?: number;
     power2?: number;
-
-    constructor(time: Date, power1: number) {
-        this.time = time;
-        this.power1 = power1;
-    }
 }
--- src/utils/PowerAnalysisUtils.test.ts
+++ src/utils/PowerAnalysisUtils.test.ts
@@ -3,37 +3,45 @@ import { render } from '@testing-library/react';
 import PowerAnalysisUtils from './PowerAnalysisUtils';
 import DataEntry from '../DataEntry';
 
+function createDataEntry(date: Date, power: number) {
+    let dataEntry = new DataEntry();
+    dataEntry.time = date;
+    dataEntry.power1 = power;
+    return dataEntry;
+}
+
 test('getMaxPower returns max power', () => {
     let dataEntries: DataEntry[] = []
-    dataEntries.push(new DataEntry(new Date(), 1));
-    dataEntries.push(new DataEntry(new Date(), 3));
-    dataEntries.push(new DataEntry(new Date(), 2));
+    dataEntries.push(createDataEntry(new Date(), 1));
+    dataEntries.push(createDataEntry(new Date(), 3));
+    dataEntries.push(createDataEntry(new Date(), 2));
 
     expect(PowerAnalysisUtils.getMaxPower(dataEntries)).toBe(3);
 });
 
 test('calculateAveragePower returns average power', () => {
     let dataEntries: DataEntry[] = []
-    dataEntries.push(new DataEntry(new Date(), 10));
-    dataEntries.push(new DataEntry(new Date(), 20));
-    dataEntries.push(new DataEntry(new Date(), 30));
+    dataEntries.push(createDataEntry(new Date(), 10));
+    dataEntries.push(createDataEntry(new Date(), 20));
+    dataEntries.push(createDataEntry(new Date(), 30));
 
     expect(PowerAnalysisUtils.calculateAveragePower(dataEntries)).toBe(20);
 });
 
 test('calculateMaxAveragePowerByTime returns average power for ten seconds', () => {
     let dataEntries: DataEntry[] = []
-    dataEntries.push(new DataEntry(new Date(), 20));  // 1
-    dataEntries.push(new DataEntry(new Date(), 20));  // 2
-    dataEntries.push(new DataEntry(new Date(), 20));  // 3
-    dataEntries.push(new DataEntry(new Date(), 20));  // 4
-    dataEntries.push(new DataEntry(new Date(), 20));  // 5
-    dataEntries.push(new DataEntry(new Date(), 10));  // 6
-    dataEntries.push(new DataEntry(new Date(), 10));  // 7
-    dataEntries.push(new DataEntry(new Date(), 10));  // 8
-    dataEntries.push(new DataEntry(new Date(), 10));  // 9
-    dataEntries.push(new DataEntry(new Date(), 10));  // 10
-    dataEntries.push(new DataEntry(new Date(), 1));   // 11
+    dataEntries.push(createDataEntry(new Date(), 615));  // 0
+    dataEntries.push(createDataEntry(new Date(), 835));  // 1
+    dataEntries.push(createDataEntry(new Date(), 886));  // 2
+    dataEntries.push(createDataEntry(new Date(), 939));  // 3
+    dataEntries.push(createDataEntry(new Date(), 948));  // 4
+    dataEntries.push(createDataEntry(new Date(), 925));  // 5
+    dataEntries.push(createDataEntry(new Date(), 861));  // 6
+    dataEntries.push(createDataEntry(new Date(), 888));  // 7
+    dataEntries.push(createDataEntry(new Date(), 785));  // 8
+    dataEntries.push(createDataEntry(new Date(), 781));  // 9
+    dataEntries.push(createDataEntry(new Date(), 724));  // 10
+    dataEntries.push(createDataEntry(new Date(), 673));  // 11
 
-    expect(PowerAnalysisUtils.calculateMaxAveragePowerByTime(dataEntries, 10)).toBe(15);
+    expect(PowerAnalysisUtils.calculateMaxAveragePowerByTime(dataEntries, 10)).toBe(857);
 })
