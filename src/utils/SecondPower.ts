5752933632368609c89c4ad5c209abf3d03be706
Jordan Terry <jordan.terry@theguardian.com>
2020-07-04 15:53:03 +0100

    Unsaved file.

--- src/utils/SecondPower.ts
+++ src/utils/SecondPower.ts
@@ -1,6 +1,6 @@
 import DataEntry from "../DataEntry"
 
-export default class SecondPower {
+export default class AverageSecondPower {
 
     private readonly start: DataEntry
     private readonly end: DataEntry
@@ -9,6 +9,6 @@ export default class SecondPower {
     constructor(start: DataEntry, end: DataEntry) {
         this.start = start
         this.end = end
-        this.average = (this.start?.power1 || 0) + (this.end?.power2 || 0) / 2
+        this.average = (this.start?.power1 || 0) + (this.end?.power1 || 0) / 2
     }
 }
