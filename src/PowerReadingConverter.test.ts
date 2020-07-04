6d37352511633b9f931ab7a86ca09420d0dfd53a
Jordan Terry <jordan.terry@theguardian.com>
2020-07-04 18:41:09 +0100

    Add a test to confirm that the number of average readings produced is one less than the DataEntry provided.

--- src/PowerReadingConverter.test.ts
+++ src/PowerReadingConverter.test.ts
@@ -16,7 +16,6 @@ test('confirm the average of two DataEntry with the power of 1 is 1', () => {
     expect(averages[0].average).toBe(1)
 });
 
-
 test('confirm the average of a DataEntry with the power of 1 and an entry with no power is 0.5', () => {
     var entryOne = new DataEntry()
     entryOne.power1 = 1
@@ -28,4 +27,14 @@ test('confirm the average of a DataEntry with the power of 1 and an entry with n
 
     expect(averages.length).toBe(1)
     expect(averages[0].average).toBe(0.5)
+});
+
+
+test('confirm that 11 DataEntry are converted to 10 average powers', () => {
+    var dataEntry = new DataEntry()
+    dataEntry.power1 = 1
+    let dataEntries: DataEntry[] = [dataEntry, dataEntry, dataEntry, dataEntry, dataEntry, dataEntry, dataEntry, dataEntry, dataEntry, dataEntry, dataEntry]
+    var powerReadingConverter = new PowerReadingConverter()
+    var averages = powerReadingConverter.convert(dataEntries)
+    expect(averages.length).toBe(10)
 });
