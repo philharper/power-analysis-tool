ee757f6bf8dd9adcafe51f38351cd7a23ec087b5
Jordan Terry <jordan.terry@theguardian.com>
2020-07-04 15:51:43 +0100

    Refactor into Converter class rather than using a utils function. This will make the code more focused and easier to reason with.

---
+++ src/PowerReadingConverter.ts
@@ -0,0 +1,24 @@
+import DataEntry from "./DataEntry";
+import AverageSecondPower from "./utils/SecondPower";
+
+export class PowerReadingConverter {
+   /**
+     * Convert an array of DataEntry to an array contaning the average power of one second.
+     * 
+     * An average power of one second is calculated by taking the power at the start and end of a second in time and then calculating the 
+     * average. 
+     * 
+     * TODO(jordan): Use the time of a DataEntry to identify a second.
+     *
+     * @param readings data entry readings to convert to second data
+     */
+    public convert(dataEntries: DataEntry[]): AverageSecondPower[] {
+        return dataEntries.reduce(function(second: AverageSecondPower[], currentValue: DataEntry, currentIndex: number, array: DataEntry[]) {
+            var nextIndex = currentIndex + 1
+            if (nextIndex < array.length) {
+                second.push(new AverageSecondPower(currentValue, array[nextIndex]))
+            }
+            return second;
+        }, new Array<AverageSecondPower>())
+    }
+}
--- src/utils/PowerAnalysisUtils.ts
+++ src/utils/PowerAnalysisUtils.ts
@@ -52,24 +52,4 @@ export default class PowerAnalysisUtils {
 
         return rangeTotalPower;
     }
-
-    /**
-     * Convert an array of DataEntry to an array contaning the average power of one second.
-     * 
-     * An average power of one second is calculated by taking the power at the start and end of a second in time and then calculating the 
-     * average. 
-     * 
-     * TODO(jordan): Use the time of a DataEntry to identify a second.
-     *
-     * @param readings data entry readings to convert to second data
-     */
-    static convertPowerReadingsToSecondData(readings: DataEntry[]): AverageSecondPower[] {
-        return readings.reduce(function(second: AverageSecondPower[], currentValue: DataEntry, currentIndex: number, array: DataEntry[]) {
-            var nextIndex = currentIndex + 1
-            if (nextIndex < array.length) {
-                second.push(new AverageSecondPower(currentValue, array[nextIndex]))
-            }
-            return second;
-        }, new Array<AverageSecondPower>())
-    }
 }
